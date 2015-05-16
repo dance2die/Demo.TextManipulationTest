c3c68a64c61b84c6965444e036f79c5c814164c2
dance2die <dontbothermewithspam@gmail.com>
2015-05-16 15:43:01 -0400

    Finished implementing text zip

---
+++ Demo.TextManipulationTest/Demo.TextManipulationTest.ZipText/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Demo.TextManipulationTest.ZipText
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var text = "This is a tweet to zip!";
+			byte[] zipped = TextZip.Zip(text);
+			DumpZipped(zipped);
+
+			var unzippedText = TextZip.Unzip(zipped);
+			Console.WriteLine("UnZipped: {0}", unzippedText);
+		}
+
+		private static void DumpZipped(byte[] zipped)
+		{
+			Console.WriteLine(string.Join(", ", zipped));
+		}
+	}
+}
